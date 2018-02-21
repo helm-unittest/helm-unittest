794f5787e9e8ac4b454ac338cb356e4bda69a740
Patrick Liu <zeldalink0515@gmail.com>
2018-02-22 01:50:41 +0800

    Fix TrustedMarshalYAML comment typo

--- unittest/common/utilities.go
+++ unittest/common/utilities.go
@@ -2,7 +2,7 @@ package common
 
 import yaml "gopkg.in/yaml.v2"
 
-// TrustedMarshalYAML malshal yaml without error return, if error happen it panic
+// TrustedMarshalYAML marshal yaml without error returned, if an error happens it panics
 func TrustedMarshalYAML(d interface{}) string {
 	s, err := yaml.Marshal(d)
 	if err != nil {
