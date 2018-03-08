2a1cb5aee8105a0e93ebbf6c5377710da713e60c
Patrick Liu <zeldalink0515@gmail.com>
2018-03-08 15:02:19 +0800

    Fix test suite test in CI, add suite fail test

--- unittest/test_suite_test.go
+++ unittest/test_suite_test.go
@@ -1,7 +1,8 @@
 package unittest_test
 
 import (
-	"os"
+	"io/ioutil"
+	"path"
 	"testing"
 
 	. "github.com/lrills/helm-unittest/unittest"
@@ -10,6 +11,8 @@ import (
 	yaml "gopkg.in/yaml.v2"
 	"k8s.io/helm/pkg/chartutil"
 )
+
+var tmpdir, _ = ioutil.TempDir("", "_suite_tests")
 
 func TestParseTestSuiteFileOk(t *testing.T) {
 	a := assert.New(t)
@@ -21,27 +24,60 @@ func TestParseTestSuiteFileOk(t *testing.T) {
 	a.Equal(s.Tests[0].Name, "should pass all kinds of assertion")
 }
 
-func TestRunSuiteOk(t *testing.T) {
+func TestRunSuiteWhenPass(t *testing.T) {
 	c, _ := chartutil.Load("../__fixtures__/basic")
 	suiteDoc := `
 suite: test suite name
 templates:
   - deployment.yaml
 tests:
-  - it: should ...
+  - it: should pass
     asserts:
       - equal:
           path: kind
           value: Deployment
-  `
+      - matchSnapshot: {}
+`
 	testSuite := TestSuite{}
 	yaml.Unmarshal([]byte(suiteDoc), &testSuite)
 
-	cache, _ := snapshot.CreateSnapshotOfSuite(os.TempDir()+"my_test.yaml", false)
+	cache, _ := snapshot.CreateSnapshotOfSuite(path.Join(tmpdir, "my_test.yaml"), false)
 	suiteResult := testSuite.Run(c, cache, &TestSuiteResult{})
 
 	a := assert.New(t)
 	a.True(suiteResult.Passed)
 	a.Nil(suiteResult.ExecError)
 	a.Equal(1, len(suiteResult.TestsResult))
+	a.Equal("test suite name", suiteResult.DisplayName)
+
+	a.Equal(uint(1), suiteResult.SnapshotCounting.Created)
+	a.Equal(uint(1), suiteResult.SnapshotCounting.Total)
+	a.Equal(uint(0), suiteResult.SnapshotCounting.Failed)
+	a.Equal(uint(0), suiteResult.SnapshotCounting.Vanished)
+}
+
+func TestRunSuiteWhenFail(t *testing.T) {
+	c, _ := chartutil.Load("../__fixtures__/basic")
+	suiteDoc := `
+suite: test suite name
+templates:
+  - deployment.yaml
+tests:
+  - it: should fail
+    asserts:
+      - equal:
+          path: kind
+          value: Pod
+`
+	testSuite := TestSuite{}
+	yaml.Unmarshal([]byte(suiteDoc), &testSuite)
+
+	cache, _ := snapshot.CreateSnapshotOfSuite(path.Join(tmpdir, "my_test.yaml"), false)
+	suiteResult := testSuite.Run(c, cache, &TestSuiteResult{})
+
+	a := assert.New(t)
+	a.False(suiteResult.Passed)
+	a.Nil(suiteResult.ExecError)
+	a.Equal(1, len(suiteResult.TestsResult))
+	a.Equal("test suite name", suiteResult.DisplayName)
 }
