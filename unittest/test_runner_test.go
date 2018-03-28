557566ef20b3400f489efa2a966c2f6afd513dd1
Patrick Liu <zeldalink0515@gmail.com>
2018-03-28 12:39:05 +0800

    Add runner test with subchart and snapshot output

--- unittest/test_runner_test.go
+++ unittest/test_runner_test.go
@@ -1,31 +1,64 @@
 package unittest_test
 
 import (
-	"os"
+	"bytes"
 	"testing"
 
+	"github.com/bradleyjkemp/cupaloy"
 	. "github.com/lrills/helm-unittest/unittest"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTestRunnerOkWithPassedTests(t *testing.T) {
+func TestRunnerOkWithPassedTests(t *testing.T) {
+	buffer := new(bytes.Buffer)
 	runner := TestRunner{
-		Printer: NewPrinter(os.Stdout, nil),
+		Printer: NewPrinter(buffer, nil),
 		Config: TestConfig{
 			TestFiles: []string{"tests/*_test.yaml"},
 		},
 	}
 	passed := runner.Run([]string{"../__fixtures__/basic"})
 	assert.True(t, passed)
+	cupaloy.SnapshotT(t, buffer.String())
 }
 
-func TestTestRunnerOkWithFailedTests(t *testing.T) {
+func TestRunnerOkWithFailedTests(t *testing.T) {
+	buffer := new(bytes.Buffer)
 	runner := TestRunner{
-		Printer: NewPrinter(os.Stdout, nil),
+		Printer: NewPrinter(buffer, nil),
 		Config: TestConfig{
 			TestFiles: []string{"tests_failed/*_test.yaml"},
 		},
 	}
 	passed := runner.Run([]string{"../__fixtures__/basic"})
 	assert.False(t, passed)
+	cupaloy.SnapshotT(t, buffer.String())
+}
+
+func TestRunnerWithTestsInSubchart(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	runner := TestRunner{
+		Printer: NewPrinter(buffer, nil),
+		Config: TestConfig{
+			WithSubChart: true,
+			TestFiles:    []string{"tests/*_test.yaml"},
+		},
+	}
+	passed := runner.Run([]string{"../__fixtures__/with-subchart"})
+	assert.True(t, passed)
+	cupaloy.SnapshotT(t, buffer.String())
+}
+
+func TestRunnerWithTestsInSubchartButFlagFalse(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	runner := TestRunner{
+		Printer: NewPrinter(buffer, nil),
+		Config: TestConfig{
+			WithSubChart: false,
+			TestFiles:    []string{"tests/*_test.yaml"},
+		},
+	}
+	passed := runner.Run([]string{"../__fixtures__/with-subchart"})
+	assert.True(t, passed)
+	cupaloy.SnapshotT(t, buffer.String())
 }
