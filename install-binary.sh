beeed6101ff7a6a7489f21fcbc1bb63d13a239dc
Quintush <2246480+quintush@users.noreply.github.com>
2020-04-29 13:47:36 +0200

    Only remove directory when it exists.

--- install-binary.sh
+++ install-binary.sh
@@ -99,7 +99,7 @@ getDownloadURL() {
 # for that binary.
 downloadFile() {
   PLUGIN_TMP_FOLDER="/tmp/_dist/"
-  rm -r $PLUGIN_TMP_FOLDER >/dev/null 2>&1
+  [ -d $PLUGIN_TMP_FOLDER ] && rm -r $PLUGIN_TMP_FOLDER >/dev/null
   mkdir -p "$PLUGIN_TMP_FOLDER"
   echo "Downloading $DOWNLOAD_URL to location $PLUGIN_TMP_FOLDER"
   if type "curl" >/dev/null 2>&1; then
