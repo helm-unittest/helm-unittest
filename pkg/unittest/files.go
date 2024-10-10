da2810043a67dc0216950e288f7a870584e2b922
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-10-10 21:02:05 +0100

    issue-300: write helpers and tests
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/unittest/files.go
+++ pkg/unittest/files.go
@@ -3,7 +3,6 @@ package unittest
 import (
 	"path/filepath"
 	"slices"
-	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/yargevad/filepathx"
@@ -22,34 +21,53 @@ const LOG_UTILS = "utils"
 // It returns a slice of file paths and an error if any occurred during processing.
 func GetFiles(chartPath string, filePatterns []string, setAbsolute bool) ([]string, error) {
 	log.WithField(LOG_UTILS, "get-files").Debugln("file-patterns:", filePatterns)
-	var filesSet []string
-	basePath := chartPath + "/" // Prepend chartPath with slash
+	// var filesSet []string
+	// basePath := chartPath + "/" // Prepend chartPath with slash
 
+	// for _, pattern := range slices.Compact(filePatterns) {
+	// 	if filepath.IsAbs(pattern) {
+	// 		filesSet = append(filesSet, pattern) // Append absolute paths directly
+	// 	} else {
+	// 		var filePath string
+	// 		if strings.Contains(pattern, basePath) {
+	// 			filePath = pattern
+	// 		} else {
+	// 			filePath = filepath.Join(basePath, pattern)
+	// 		}
+	// 		files, err := filepathx.Glob(filePath)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 		filesSet = append(filesSet, files...) // Append all files (relative)
+	// 	}
+	// }
+
+	// if setAbsolute {
+	// 	// If setAbsolute is true, convert the file paths to absolute paths
+	// 	for i, filePath := range filesSet {
+	// 		if !filepath.IsAbs(filePath) {
+	// 			absPath, _ := filepath.Abs(filePath)
+	// 			filesSet[i] = absPath
+	// 		}
+	// 	}
+	// }
+	filesSet := make([]string, 0)
 	for _, pattern := range slices.Compact(filePatterns) {
-		if filepath.IsAbs(pattern) {
-			filesSet = append(filesSet, pattern) // Append absolute paths directly
-		} else {
-			var filePath string
-			if strings.Contains(pattern, basePath) {
-				filePath = pattern
-			} else {
-				filePath = filepath.Join(basePath, pattern)
-			}
-			files, err := filepathx.Glob(filePath)
+		if !filepath.IsAbs(pattern) {
+			files, err := filepathx.Glob(filepath.Join(chartPath, pattern))
 			if err != nil {
 				return nil, err
 			}
-			filesSet = append(filesSet, files...) // Append all files (relative)
-		}
-	}
-
-	if setAbsolute {
-		// If setAbsolute is true, convert the file paths to absolute paths
-		for i, filePath := range filesSet {
-			if !filepath.IsAbs(filePath) {
-				absPath, _ := filepath.Abs(filePath)
-				filesSet[i] = absPath
+			if setAbsolute {
+				for _, file := range files {
+					file, _ = filepath.Abs(file)
+					filesSet = append(filesSet, file)
+				}
+			} else {
+				filesSet = append(filesSet, files...)
 			}
+		} else {
+			filesSet = append(filesSet, pattern)
 		}
 	}
 
