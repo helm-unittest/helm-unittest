4e66d03d5b8a186efb323be0a9f673e5becee327
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-12-21 19:28:52 +0000

    feat(issue-504): test_runner output with tests
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/unittest/test_runner_test.go
+++ pkg/unittest/test_runner_test.go
@@ -2,16 +2,19 @@ package unittest_test
 
 import (
 	"bytes"
+	"os"
 	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
 	"testing"
+	"testing/fstest"
 
 	"github.com/bradleyjkemp/cupaloy/v2"
 	"github.com/helm-unittest/helm-unittest/internal/printer"
 	. "github.com/helm-unittest/helm-unittest/pkg/unittest"
 	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/yaml"
 )
 
 var sectionBeginPattern = regexp.MustCompile("( PASS | FAIL |\n*###|\n*Charts:|\n*Snapshot Summary:)")
@@ -271,4 +274,171 @@ func TestV3RunnerOkWithSchemaValidation(t *testing.T) {
 	passed := runner.RunV3([]string{testV3WithSchemaChart})
 	assert.True(t, passed, buffer.String())
 	cupaloy.SnapshotT(t, makeOutputSnapshotable(buffer.String())...)
+}
+
+func TestV3RunnerOkWithTestSkipped(t *testing.T) {
+	suiteDoc := `
+suite: test suite with subchart
+templates:
+  - charts/postgresql/templates/deployment.yaml
+tests:
+  - it: should pass
+    asserts:
+      - equal:
+          path: kind
+          value: Deployment
+      - matchSnapshot: {}
+`
+	testSuite := TestSuite{}
+	err := yaml.Unmarshal([]byte(suiteDoc), &testSuite)
+	assert.Nil(t, err)
+
+	buffer := new(bytes.Buffer)
+	runner := TestRunner{
+		Printer:   printer.NewPrinter(buffer, nil),
+		TestFiles: []string{testTestFiles},
+	}
+	passed := runner.RunV3([]string{testV3WithSchemaChart})
+	assert.True(t, passed, buffer.String())
+}
+
+func TestV3RunnerOkWithSkippedTests_Output(t *testing.T) {
+	chart := `
+apiVersion: v2
+name: basic
+version: 0.1.0
+`
+	deployment := `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx
+spec:
+  replicas: 3
+`
+	firstTest := `
+---
+suite: test skip on suite level
+templates:
+  - deployment.yaml
+tests:
+  - it: should render deployment
+    asserts:
+      - exists:
+          path: spec.replicas
+`
+	secondTest := `
+---
+suite: test skip on suite level
+templates:
+  - deployment.yaml
+skip:
+  reason: "This suite is not ready yet"
+tests:
+  - it: should render deployment
+    asserts:
+      - exists:
+          path: spec.replicas
+`
+
+	thirdFailedTest := `
+---
+suite: test skip on suite level
+templates:
+  - deployment.yaml
+tests:
+  - it: should render deployment
+    asserts:
+      - exists:
+          path: spec.notExists
+`
+
+	tmp := t.TempDir()
+	paths := []string{filepath.Join(tmp, "chart/templates"), filepath.Join(tmp, "chart/tests")}
+	for _, path := range paths {
+		err := os.MkdirAll(path, 0755)
+		assert.NoError(t, err)
+	}
+
+	fs := fstest.MapFS{
+		"chart/Chart.yaml":                   {Data: []byte(chart)},
+		"chart/templates/deployment.yaml":    {Data: []byte(deployment)},
+		"chart/tests/deployment_test.yaml":   {Data: []byte(firstTest)},
+		"chart/tests/deployment_2_test.yaml": {Data: []byte(secondTest)},
+		"chart/tests/deployment_3_test.yaml": {Data: []byte(thirdFailedTest)},
+	}
+
+	for path, el := range fs {
+		err := os.WriteFile(filepath.Join(tmp, path), el.Data, 0644)
+		assert.NoError(t, err)
+	}
+	buffer := new(bytes.Buffer)
+	runner := TestRunner{
+		Printer:   printer.NewPrinter(buffer, nil),
+		TestFiles: []string{testTestFiles},
+	}
+	_ = runner.RunV3([]string{filepath.Join(tmp, "chart")})
+
+	assert.Contains(t, buffer.String(), "Test Suites: 1 failed, 1 passed, 1 skipped, 3 total")
+	assert.Contains(t, buffer.String(), "- SKIPPED 'should render deployment'")
+	assert.Contains(t, buffer.String(), "Tests:       1 failed, 1 passed, 1 skipped, 3 total")
+}
+
+func TestV3RunnerOkWithSkippedSuits_Output(t *testing.T) {
+	chart := `
+apiVersion: v2
+name: basic
+version: 0.1.0
+`
+	deployment := `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx
+spec:
+  replicas: 3
+`
+	firstTest := `
+---
+suite: should skip one and execute one
+templates:
+  - deployment.yaml
+tests:
+  - it: should skip test
+    skip:
+     reason: "This suite is not ready yet"
+    asserts:
+      - exists:
+          path: metadata.name
+  - it: should not skip test
+    asserts:
+      - exists:
+          path: metadata.name
+`
+	tmp := t.TempDir()
+	paths := []string{filepath.Join(tmp, "chart/templates"), filepath.Join(tmp, "chart/tests")}
+	for _, path := range paths {
+		err := os.MkdirAll(path, 0755)
+		assert.NoError(t, err)
+	}
+
+	fs := fstest.MapFS{
+		"chart/Chart.yaml":                 {Data: []byte(chart)},
+		"chart/templates/deployment.yaml":  {Data: []byte(deployment)},
+		"chart/tests/deployment_test.yaml": {Data: []byte(firstTest)},
+	}
+
+	for path, el := range fs {
+		err := os.WriteFile(filepath.Join(tmp, path), el.Data, 0644)
+		assert.NoError(t, err)
+	}
+	buffer := new(bytes.Buffer)
+	runner := TestRunner{
+		Printer:   printer.NewPrinter(buffer, nil),
+		TestFiles: []string{testTestFiles},
+	}
+	_ = runner.RunV3([]string{filepath.Join(tmp, "chart")})
+	assert.Contains(t, buffer.String(), "PASS  should skip one and execute one")
+	assert.Contains(t, buffer.String(), "- SKIPPED 'should skip test'")
+	assert.Contains(t, buffer.String(), "Tests:       1 passed, 1 skipped, 2 total")
 }
