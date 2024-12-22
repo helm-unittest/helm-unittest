22eb47ea79aa3bec296d126155b340f2f03ea1f4
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-12-22 19:25:24 +0000

    feat(issue-499): create test fixtures
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

---
+++ pkg/unittest/fixtures/chart01/templates/validation.tpl
@@ -0,0 +1,1 @@
+{{- fail (printf "`runAsNonRoot` is set to `true` but `runAsUser` is set to `0` (root)") }}
---
+++ pkg/unittest/test_runner_fixture_test.go
@@ -0,0 +1,21 @@
+package unittest
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/helm-unittest/helm-unittest/internal/printer"
+	"github.com/stretchr/testify/assert"
+)
+
+const fixtureChart01 string = "fixtures/chart01"
+
+func TestV3RunnerOkWith_Fixture_Chart01(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	runner := TestRunner{
+		Printer:   printer.NewPrinter(buffer, nil),
+		TestFiles: []string{"tests/*_test.yaml"},
+	}
+	passed := runner.RunV3([]string{fixtureChart01})
+	assert.True(t, passed, buffer.String())
+}
