17e69cfadc676430c64c189382b9596aaa780133
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-12-31 10:44:25 +0000

    merge with main prepare
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- internal/common/utilities.go
+++ internal/common/utilities.go
@@ -7,10 +7,6 @@ import (
 	yaml "gopkg.in/yaml.v3"
 )
 
-const (
-	bsCode byte = byte('\\')
-)
-
 // TrustedMarshalYAML marshal yaml without error returned, if an error happens it panics
 func TrustedMarshalYAML(d interface{}) string {
 	byteBuffer := new(bytes.Buffer)
@@ -31,46 +27,4 @@ func TrustedUnmarshalYAML(d string) map[string]interface{} {
 		panic(err)
 	}
 	return parsedYaml
-}
-
-type YmlEscapeHandlers struct{}
-
-// Escape function is required, as yaml library no longer maintained
-// yaml unmaintained library issue https://github.com/go-yaml/yaml/pull/862
-func (y *YmlEscapeHandlers) Escape(content string) []byte {
-	if !strings.Contains(content, `\`) {
-		return nil
-	}
-	return escapeBackslashes([]byte(content))
-}
-
-// escapeBackslashes escapes backslashes in the given byte slice.
-// It ensures that an even number of backslashes are present by doubling any single backslash found.
-func escapeBackslashes(content []byte) []byte {
-	var result bytes.Buffer
-	for i := 0; i < len(content); i++ {
-		if content[i] != bsCode {
-			result.WriteByte(content[i])
-			continue
-		}
-		count := 1
-		for i+count < len(content) && content[i+count] == bsCode {
-			count++
-		}
-
-		times := count
-		if count%2 == 1 {
-			times = count + 1
-		}
-
-		if count > 1 {
-			i += count - 1
-		}
-
-		for j := 0; j < times; j++ {
-			result.WriteByte('\\')
-		}
-
-	}
-	return result.Bytes()
 }
