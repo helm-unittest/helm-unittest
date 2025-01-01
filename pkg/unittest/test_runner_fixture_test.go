34bcbea50835fb5a312dee6eafcfb7069e3eccbe
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-01-01 12:04:27 +0000

    wip
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/unittest/test_runner_fixture_test.go
+++ pkg/unittest/test_runner_fixture_test.go
@@ -8,14 +8,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const fixtureChart01 string = "test_fixtures/chart01"
-
 func TestV3RunnerWith_Fixture_Chart01(t *testing.T) {
 	buffer := new(bytes.Buffer)
 	runner := TestRunner{
 		Printer:   printer.NewPrinter(buffer, nil),
 		TestFiles: []string{"tests/*_test.yaml"},
 	}
-	passed := runner.RunV3([]string{fixtureChart01})
+	passed := runner.RunV3([]string{"testdata/chart01"})
 	assert.True(t, passed, buffer.String())
 }
