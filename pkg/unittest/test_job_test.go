28c4e5184564e9244988ec7540b81f73511cc98d
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-01-09 08:14:00 +0000

    issue-501: prepare for merge
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/unittest/test_job_test.go
+++ pkg/unittest/test_job_test.go
@@ -814,7 +814,7 @@ asserts:
 	assert.True(testResult.Passed, testResult.AssertsResult)
 }
 
-func TestV3RunJobWithNotSupportedAssert(t *testing.T) {
+func TestV3RunJobWithWithNotSupportedAssert(t *testing.T) {
 	c, _ := loader.Load(testV3BasicChart)
 	manifest := `
 it: should skip when not supported assert is found
@@ -927,24 +927,4 @@ ingress:
 	assert.Nil(t, testResult.ExecError)
 	assert.True(t, testResult.Passed)
 	assert.Equal(t, 3, len(testResult.AssertsResult))
-}
-
-func TestV3RunSkippedJob(t *testing.T) {
-	c, _ := loader.Load(testV3BasicChart)
-	manifest := `
-it: should skip when not supported assert is found
-template: templates/deployment.yaml
-documentIndex: 0
-skip:
- reason: "Skip this test"
-asserts:
-  - equal:
-      path: kind
-      value: Deployment
-`
-	var tj TestJob
-	common.YmlUnmarshalTestHelper(manifest, &tj, t)
-
-	testResult := tj.RunV3(c, nil, true, "", &results.TestJobResult{})
-	assert.True(t, testResult.Skipped)
 }
