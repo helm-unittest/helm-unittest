4621a3ec2360a20a720770c0982a219a7ba9a144
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2025-01-28 00:06:32 +0000

    feat(skip-test): issue-501 added pkg/tesdata chart with tests
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- pkg/unittest/files_test.go
+++ pkg/unittest/files_test.go
@@ -43,7 +43,7 @@ func TestGetFiles_ChartWithoutSubCharts(t *testing.T) {
 
 	actual, err := GetFiles(".", []string{"tests/*_test.yaml"}, false)
 	assert.NoError(t, err)
-	assert.Equal(t, len(actual), 12)
+	assert.Equal(t, len(actual), 13)
 }
 
 func TestGetFiles_ChartWithoutSubChartsNoDuplicates(t *testing.T) {
