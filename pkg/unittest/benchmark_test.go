ef742b14a8ff3d532cedebc79bc07901b18efd2c
quintush <qvanhoudt@outlook.com>
2025-09-08 23:24:51 +0200

    Restructure benchmark test and cleanup testduplications.

--- pkg/unittest/benchmark_test.go
+++ pkg/unittest/benchmark_test.go
@@ -1,6 +1,7 @@
 package unittest_test
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"runtime"
@@ -18,6 +19,35 @@ import (
 // and cleans up the generated files after the benchmark completes.
 // The test uses a silent printer to avoid output overhead during benchmarking.
 func BenchmarkNewTestForCPUAndMemory(b *testing.B) {
+	// Copy the sample test file 400 times to simulate a large number of test files
+	const numCopies = 400
+	const sampleTestFile = "testdata/chart-benchmark/tests/main_test.yaml"
+	const testDir = "testdata/chart-benchmark/tests/"
+
+	// Ensure the test directory exists
+	if _, err := os.Stat(testDir); os.IsNotExist(err) {
+		err := os.MkdirAll(testDir, os.ModePerm)
+		assert.NoError(b, err)
+	}
+
+	// Create copies of the sample test file
+	files := make([]string, numCopies)
+	for i := range numCopies {
+		destFile := fmt.Sprintf("%s%d%s", testDir, i, "main_test.yaml")
+		files[i] = destFile
+		input, err := os.ReadFile(sampleTestFile)
+		assert.NoError(b, err)
+		err = os.WriteFile(destFile, input, 0644)
+		assert.NoError(b, err)
+	}
+
+	b.Cleanup(func() {
+		for _, file := range files {
+			err := os.Remove(file)
+			assert.NoError(b, err)
+		}
+	})
+
 	runner := TestRunner{
 		Printer:   printer.NewPrinter(io.Discard, nil),
 		TestFiles: []string{"tests/*_test.yaml"},
